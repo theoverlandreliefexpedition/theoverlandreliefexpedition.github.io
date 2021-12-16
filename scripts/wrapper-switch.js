a4f95f0d6876f1896b5ed625adb7eedc1a2cbe5e
theoverlandreliefexpedition <95928250+theoverlandreliefexpedition@users.noreply.github.com>
2021-12-16 14:28:36 +0100

    Delete scripts directory

--- scripts/wrapper-switch.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('button-end').addEventListener('click', function(){
-	
-	document.getElementById('wrapper').style.display = 'none';
-	document.getElementById('wrapper2').style.display = 'flex';
-	document.getElementById('audio-2').play();
-	
-});
-
