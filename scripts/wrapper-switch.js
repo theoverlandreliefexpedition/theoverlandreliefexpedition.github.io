123b14d26fcdf350b962460a354e37d097859e5f
theoverlandreliefexpedition <95928250+theoverlandreliefexpedition@users.noreply.github.com>
2021-12-17 18:05:04 +0100

    Add files via upload

--- scripts/wrapper-switch.js
+++ scripts/wrapper-switch.js
@@ -6,3 +6,12 @@ document.getElementById('button-end').addEventListener('click', function () {
   document.getElementById('audio-2').play();
 
 });
+
+document.getElementById('button-again').addEventListener('click', function () {
+
+  document.getElementById('wrapper2').style.display = 'none';
+  document.getElementById('navigation').style.display = 'fixed';
+  document.getElementById('wrapper').style.display = 'flex';
+  document.getElementById('audioplayer').play();
+
+});
