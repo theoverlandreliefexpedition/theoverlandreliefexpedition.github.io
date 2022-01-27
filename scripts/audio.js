6eedbf06714eead190fb4ef185cc6c40898da0ee
henrikstelter <stelterhenrik@gmail.com>
2022-01-27 12:31:49 +0100

    changing audio

---
+++ scripts/audio.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+var audioplayers = document.querySelectorAll('.audioplayer');
+
+var startPlayback = function(e){
+	for( audioplayer of audioplayers ) {
+
+		// den aktuellen audioplayer nicht pausieren:
+		if( audioplayer === this ) {
+			return;
+		}
+
+		audioplayer.pause();
+		// audioplayer.currentTime = 0; // wieder von anfang an starten, statt der aktuellen position
+	}
+};
+
+
+for( audioplayer of audioplayers ){
+	audioplayer.addEventListener( 'play', startPlayback );
+}
+
+
+var outerWrapper = document.getElementById('outer-wrapper');
+var screen = 1;
+
+outerWrapper.addEventListener('scroll', function(){
+
+	var scrollPosition = outerWrapper.scrollTop;
+	var screenWidth = window.innerWidth;
+
+	var currentScreen = Math.ceil(scrollPosition/screenWidth);
+
+	if( currentScreen != screen ) {
+
+		// alle audioplayer stoppen:
+		for( audioplayer of audioplayers ) {
+			audioplayer.pause();
+		}
+
+		// nächsten Player starten:
+		var nextPlayer = document.getElementById('audio-'+currentScreen);
+		if( nextPlayer ) {
+			nextPlayer.play();
+		}
+
+		screen = currentScreen;
+	}
+
+});
+
+
+
+});
